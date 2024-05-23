b35e40e4cf429df5549fa55e8a25db812cce72f4
Steven BazanG <ste_alex_bazan@hotmail.com>
2024-05-22 23:25:52 -0500

    actualizacion

--- ModuloSeguridad/obj/Debug/net8.0/ModuloSeguridad.AssemblyInfo.cs
+++ ModuloSeguridad/obj/Debug/net8.0/ModuloSeguridad.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModuloSeguridad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b4a01e385ace6057753471828e48ed8536051a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModuloSeguridad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModuloSeguridad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- visual/obj/Debug/net8.0/Controlador.AssemblyInfo.cs
+++ visual/obj/Debug/net8.0/Controlador.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Controlador")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b4a01e385ace6057753471828e48ed8536051a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Controlador")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Controlador")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
